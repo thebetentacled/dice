962e25dc82569ad7ed921598ef6106847af5c383
thebetentacled <dontcontact@goaway.now>
2024-05-29 08:52:18 -0400

    added license and license info

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # dice game by thebetentacled
-# (license info)
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
 
 # imports
 import random # for rolling dice

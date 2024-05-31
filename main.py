cda650a9ebc9d3338d7183ea106fd5f66d951abe
thebetentacled <dontcontact@goaway.now>
2024-05-31 12:35:45 -0400

    total revamp of functions

--- main.py
+++ main.py
@@ -6,154 +6,24 @@ import random # for rolling dice
 
 # variables
 walletCash = 20
-currentBet = ""
-currentMoneyBet = ""
-currentRoll = 0
+diceRoll = 0
 
 # functions
-def sayCash():
-    print("You have $", walletCash, ".")
+def rollDice(minRoll, maxRoll):
+    global diceRoll
+    diceRoll = random.randint(minRoll, maxRoll)
 
-def rollDice(): # rolls dice, duh
-    global curentRoll
-    global currentBet
-    global currentMoneyBet
-    currentRoll = random.randint(2, 12)
-    print("dice rolled ", currentRoll)
-    print("you bet that dice would roll", currentBet)
-    print("you bet $", currentMoneyBet, ".")
-    if currentRoll == 2:
-        printTwo()
-    elif currentRoll == 3:
-        printThree()
-    elif currentRoll == 4:
-        printFour()
-    elif currentRoll == 5:
-        printFive()
-    elif currentRoll == 6:
-        printSix()
-    elif currentRoll == 7:
-        printSeven()
-    elif currentRoll == 8:
-        printEight()
-    elif currentRoll == 9:
-        printNine()
-    elif currentRoll == 10:
-        printTen()
-    elif currentRoll == 11:
-        printEleven()
-    elif currentRoll == 12:
-        printTwelve()
-    changeMoney()
+def takeInput():
+    userGuess = int(input("what will the dice roll? -> "))
+    userBet = int(input("how much are you willing to bet on that? -> "))
+    rollDice(2, 12)
+    changeMoney(userGuess, userBet)
 
-def placeBet(): # place your bet on the next roll
-    global currentBet
-    global currentMoneyBet
-    sayCash()
-    currentBet = input("what will the next roll be? -> ")
-    print("you've bet that the dice will roll a(n) " + currentBet)
-    currentMoneyBet = input("how much will you bet on that? -> ")
-    print("you've bet $" + currentMoneyBet + " on that.")
-    rollDice()
-
-def changeMoney(): # take or add money to your wallet
+def changeMoney(guess, bet):
+    global diceRoll
     global walletCash
-    global currentBet
-    global currentMoneyBet
-    global currentRoll
-    if int(currentBet) == currentRoll:
-       walletCash = walletCash + int(currentMoneyBet)
-       placeBet()
-    elif int(currentBet) != currentRoll:
-        walletCash = walletCash - int(currentMoneyBet)
-        lossCheck()
-    else: 
-        print("what the fuck")
-
-def lossCheck(): # check if you lose or not
-    global walletCash
-    if walletCash <= 0:
-        print("you lost. better luck next time.")
-        exit()
+    if guess == diceRoll:
+        walletCash = walletCash + bet
     else:
-        placeBet()
-
-def printTwo():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("|         |  |         |")
-    print("|    O    |  |    O    |")
-    print("|         |  |         |")
-    print("|_________|  |_________|")
-
-def printThree():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("|         |  |     O   |")
-    print("|    O    |  |         |")
-    print("|         |  |   O     |")
-    print("|_________|  |_________|")
-
-def printFour():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("|   O     |  |         |")
-    print("|    O    |  |    O    |")
-    print("|     O   |  |         |")
-    print("|_________|  |_________|")
-
-def printFive():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("|     O   |  |     O   |")
-    print("|         |  |    O    |")
-    print("|   O     |  |   O     |")
-    print("|_________|  |_________|")
-
-def printSix():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("|     O   |  |     O   |")
-    print("|    O    |  |    O    |")
-    print("|   O     |  |   O     |")
-    print("|_________|  |_________|")
-
-def printSeven():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("| O     O |  |     O   |")
-    print("|         |  |    O    |")
-    print("| O     O |  |   O     |")
-    print("|_________|  |_________|")
-
-def printEight():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("|     O   |  | O     O |")
-    print("|    O    |  |    O    |")
-    print("|   O     |  | O     O |")
-    print("|_________|  |_________|")
-
-def printNine():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("| O     O |  | O     O |")
-    print("|         |  |    O    |")
-    print("| O     O |  | O     O |")
-    print("|_________|  |_________|")
-
-def printTen():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("| O     O |  | O     O |")
-    print("|    O    |  |    O    |")
-    print("| O     O |  | O     O |")
-    print("|_________|  |_________|")
-
-def printEleven():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("| O     O |  | O     O |")
-    print("| O     O |  |    O    |")
-    print("| O     O |  | O     O |")
-    print("|_________|  |_________|")
-
-def printTwelve():
-    print("|‾‾‾‾‾‾‾‾‾|  |‾‾‾‾‾‾‾‾‾|")
-    print("| O     O |  | O     O |")
-    print("| O     O |  | O     O |")
-    print("| O     O |  | O     O |")
-    print("|_________|  |_________|")
-
+        walletCash = walletCash - bet
 # main code
-placeBet()
